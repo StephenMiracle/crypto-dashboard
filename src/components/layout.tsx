f58cc5e8c9dc3a26d9da5d5ce618ade5e6bd8419
stephen miracle <miracle@stephens-MacBook-Pro.local>
2021-11-16 21:32:02 -0500

    readability updates for volume market cap.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -29,6 +29,7 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
+          maxWidth: 1400,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
