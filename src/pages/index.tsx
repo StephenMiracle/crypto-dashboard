ae52aab997d8a9f6df95c0a53e7f407f2437e6a5
stephen miracle <miracle@stephens-MacBook-Pro.local>
2021-11-17 00:00:32 -0500

    updates for readability.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -393,6 +393,22 @@ const hourlyMarketCapData = {
           </ul>
         </div>
       <div className="flex mb-4">
+        <div className="md:w-1/4">
+          <h4>Volume Data</h4>
+          <ul className="ml-0">
+            <li><span className="font-bold">Recent:</span> ${ (data?.volumeData?.current / 1000000000).toFixed(2) }B</li>
+            <li><span className="font-bold">1 day:</span> ${ (data?.volumeData?.day / 1000000000).toFixed(2) }B</li>
+            <li><span className="font-bold">7 day:</span> ${ (data?.volumeData?.week / 1000000000).toFixed(2) }B</li>
+          </ul>
+        </div>
+        <div className="md:w-1/4">
+          <h4>Market Cap Data</h4>
+          <ul className="ml-0">
+            <li><span className="font-bold">Recent:</span> ${ (data?.marketCapData?.current / 1000000000).toFixed(2) }B</li>
+            <li><span className="font-bold">24 Hour:</span> ${ (data?.marketCapData?.day / 1000000000).toFixed(2) }B</li>
+            <li><span className="font-bold">7 Day:</span> ${ (data?.marketCapData?.week / 1000000000).toFixed(2) }B</li>
+          </ul>
+        </div>
         <div className="md:w-1/2">
           <h4>Buy Signals</h4>
           <ul className="ml-0">
@@ -409,22 +425,6 @@ const hourlyMarketCapData = {
                 return <li>{signal}</li>
               })
             }
-          </ul>
-        </div>
-        <div className="md:w-1/4">
-          <h4>Volume Data</h4>
-          <ul className="ml-0">
-            <li><span className="font-bold">Recent:</span> ${ (data?.volumeData?.current / 1000000000).toFixed(2) }B</li>
-            <li><span className="font-bold">1 day:</span> ${ (data?.volumeData?.day / 1000000000).toFixed(2) }B</li>
-            <li><span className="font-bold">7 day:</span> ${ (data?.volumeData?.week / 1000000000).toFixed(2) }B</li>
-          </ul>
-        </div>
-        <div className="md:w-1/4">
-          <h4>Market Cap Data</h4>
-          <ul className="ml-0">
-            <li><span className="font-bold">Recent:</span> ${ (data?.marketCapData?.current / 1000000000).toFixed(2) }B</li>
-            <li><span className="font-bold">24 Hour:</span> ${ (data?.marketCapData?.day / 1000000000).toFixed(2) }B</li>
-            <li><span className="font-bold">7 Day:</span> ${ (data?.marketCapData?.week / 1000000000).toFixed(2) }B</li>
           </ul>
         </div>
       </div>
